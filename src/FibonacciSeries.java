e190e168d2d910101aba4f87fdef626fb3b625a2
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-24 11:01:08 +0530

    Add. Included the logic to print First n Fibonacci series.

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class FibonacciSeries {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args){
+
+            int totalNumbers;
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the number of fibonacci numbers you need: ");
+            totalNumbers = scan.nextInt();
+
+            int firstPrecedingNumber = 0, secondPrecedingNumber = 1, currentNumber;
+            int count = 2;
+            System.out.println("First "+totalNumbers+" Fibonacci numbers are");
+            System.out.println(firstPrecedingNumber+"\n"+secondPrecedingNumber);
+
+            for(; count<totalNumbers; count++)
+            {
+                currentNumber = firstPrecedingNumber+secondPrecedingNumber;
+                System.out.println(currentNumber);
+
+                firstPrecedingNumber = secondPrecedingNumber;
+                secondPrecedingNumber = currentNumber;
+            }
+
+            scan.close();
+        }
+
     }
-}
